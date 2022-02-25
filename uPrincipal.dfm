a877d420e64863a9b73e17f5c120998bc75eb568
cespinozaES <cespinoza@electrosoftware.net>
2022-02-25 12:32:45 -0500

    Se agregan botones

--- uPrincipal.dfm
+++ uPrincipal.dfm
@@ -13,6 +13,22 @@ object Form1: TForm1
   OldCreateOrder = False
   PixelsPerInch = 96
   TextHeight = 13
+  object Button1: TButton
+    Left = 80
+    Top = 56
+    Width = 75
+    Height = 25
+    Caption = 'Button1'
+    TabOrder = 0
+  end
+  object Button2: TButton
+    Left = 256
+    Top = 56
+    Width = 75
+    Height = 25
+    Caption = 'Button2'
+    TabOrder = 1
+  end
   object FDQuery1: TFDQuery
     Left = 216
     Top = 104
--- uPrincipal.pas
+++ uPrincipal.pas
@@ -7,11 +7,13 @@ uses
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, FireDAC.Stan.Intf, FireDAC.Stan.Option,
   FireDAC.Stan.Param, FireDAC.Stan.Error, FireDAC.DatS, FireDAC.Phys.Intf,
   FireDAC.DApt.Intf, FireDAC.Stan.Async, FireDAC.DApt, Data.DB,
-  FireDAC.Comp.DataSet, FireDAC.Comp.Client;
+  FireDAC.Comp.DataSet, FireDAC.Comp.Client, Vcl.StdCtrls;
 
 type
   TForm1 = class(TForm)
     FDQuery1: TFDQuery;
+    Button1: TButton;
+    Button2: TButton;
   private
     { Private declarations }
   public
